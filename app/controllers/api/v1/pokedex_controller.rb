8b2958597333f69f7c815cf8fac772a8bec6ef34
alexnsouza6 <alexnsouza6@hotmail.com>
2019-04-11 20:32:34 -0300

    feat: CRUD for pokemon entity

--- app/controllers/api/v1/pokedex_controller.rb
+++
@@ -1,3 +0,0 @@
-# PokedexController handles request for the first 151 pokemons on pokedex API
-class Api::V1::PokedexController < ApplicationController
-end
---
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -0,0 +1,43 @@
+class Api::V1::PokemonsController < ApplicationController
+  def show
+    @pokemon = Pokemon.find(params[:id])
+    render json: @pokemon, status: 200
+  end
+
+  def index
+    @pokemons = Pokemon.all
+    render json: @pokemons, status: 200
+  end
+
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+
+    if @pokemon.save
+      render json: @pokemon, status: 200
+    else
+      render json: { "error": [@pokemon.errors.full_messages] }.to_json, status: 422
+    end
+  end
+
+  def update
+    @pokemon = Pokemon.find(params[:id])
+
+    if @pokemon.update(pokemon_params)
+      render json: @pokemon, status: 200
+    else
+      render json: { "error": [@pokemon.errors.full_messages] }.to_json, status: 422
+    end
+  end
+
+  def destroy
+    @pokemon = Pokemon.find(params[:id])
+    @pokemon.destroy
+    render json: { "message": "Pokemon was successfully destroyed" }, status: 200
+
+  end
+
+  private
+  def pokemon_params
+    params.require(:pokemon).permit(:name)
+  end
+end
