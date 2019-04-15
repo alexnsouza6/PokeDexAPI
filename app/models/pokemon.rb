570fae862bc4caa4ea6a48198cb325f991ec815a
alexnsouza6 <alexnsouza6@hotmail.com>
2019-04-14 22:55:22 -0300

    feat: added comments to models

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,4 +1,6 @@
+# Pokemon model handles Pokemon associations and validations as well
 class Pokemon < ApplicationRecord
+  # Associations
   has_many :evolutions, class_name: "Pokemon",
                           foreign_key: "evolution_id"
  
@@ -7,6 +9,8 @@ class Pokemon < ApplicationRecord
   has_many :pokemon_types
   has_many :types, through: :pokemon_types
 
+  # Validations
   validates :name, presence: true
+  validates :image_url, presence: true
 
 end
