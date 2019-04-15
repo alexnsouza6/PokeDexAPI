17cf7800f248adc97b92991b4660781bb308fda8
alexnsouza6 <alexnsouza6@hotmail.com>
2019-04-14 23:44:40 -0300

    refactor: removing constants

--- spec/models/pokemon_spec.rb
+++ spec/models/pokemon_spec.rb
@@ -1,10 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 require 'task_helper'
 
 RSpec.describe Pokemon, type: :model do
-  # Constants
-  POKEMON_NUMBER = 151
-
   context 'when testing associations...' do
     it { is_expected.to have_many(:evolutions) }
     it { is_expected.to have_many(:types).through(:pokemon_types) }
@@ -16,40 +15,43 @@ RSpec.describe Pokemon, type: :model do
     it { is_expected.to validate_presence_of(:image_url) }
   end
 
-
   context 'TASK: when testing tasks...' do
-    describe "pokedex_setup:pokemon" do
-      it "saves first 151 pokemons in db" do
-        VCR.use_cassette("pokemons/species") do
-          execute_rake("pokedex_setup.rake", "pokedex_setup:pokemon")
+    describe 'pokedex_setup:pokemon' do
+      it 'saves first 151 pokemons in db' do
+        VCR.use_cassette('pokemons/species') do
+          execute_rake('pokedex_setup.rake', 'pokedex_setup:pokemon')
           expect(Pokemon.count).to eq POKEMON_NUMBER
         end
       end
-  
-      it "creates types in db" do
-        VCR.use_cassette("pokemons/species") do
-          execute_rake("pokedex_setup.rake", "pokedex_setup:pokemon")
+
+      it 'creates types in db' do
+        VCR.use_cassette('pokemons/species') do
+          execute_rake('pokedex_setup.rake', 'pokedex_setup:pokemon')
           expect(Type.all.length).to be > 0
         end
       end
     end
 
-    describe "pokedex_setup:evolution" do
-      it "adds evolutions to pokemons" do
-        VCR.use_cassette("pokemons/evolutions") do
-          type = Type.create(description: 'grass')
-          pokemon = Pokemon.create(name: 'bulbassaur', image_url: 'www.com', types: [type])
-          execute_rake("pokedex_setup.rake", "pokedex_setup:evolutions")
-          byebug
-          expect(pokemon.evolutions.length).to eq 1
+    describe 'pokedex_setup:evolution' do
+      it 'adds evolutions to pokemons' do
+        VCR.use_cassette('pokemons/species') do
+          execute_rake('pokedex_setup.rake', 'pokedex_setup:pokemon')
+          VCR.use_cassette('pokemons/evolutions') do
+            execute_rake('pokedex_setup.rake', 'pokedex_setup:evolutions')
+            pokemon = Pokemon.first
+            expect(pokemon.evolutions.length).to eq 1
+          end
         end
       end
-  
+
       it "adds evolutions to pokemon's evolution" do
-        VCR.use_cassette("pokemons/evolutions") do
-          execute_rake("pokedex_setup.rake", "pokedex_setup:evolutions")
-          evolution = Pokemon.first.evolutions.drop(1)
-          expect(evolution.evolutions.length).to eq 1
+        VCR.use_cassette('pokemons/species') do
+          execute_rake('pokedex_setup.rake', 'pokedex_setup:pokemon')
+          VCR.use_cassette('pokemons/evolutions') do
+            execute_rake('pokedex_setup.rake', 'pokedex_setup:evolutions')
+            evolution = Pokemon.first.evolutions.first
+            expect(evolution.evolutions.length).to eq 1
+          end
         end
       end
     end
