69e4c78274fb0e2df3d9072c1cc1dba5d399f895
alexnsouza6 <alexnsouza6@hotmail.com>
2019-04-15 23:05:09 -0300

    feat: mount_uploader helper

--- app/models/pokemon.rb
+++ app/models/pokemon.rb
@@ -1,16 +1,18 @@
+# frozen_string_literal: true
+
 # Pokemon model handles Pokemon associations and validations as well
 class Pokemon < ApplicationRecord
   # Associations
-  has_many :evolutions, class_name: "Pokemon",
-                          foreign_key: "evolution_id"
- 
-  belongs_to :pokemon, class_name: "Pokemon", foreign_key: "evolution_id", optional: true
+  has_many :evolutions, class_name: 'Pokemon',
+                        foreign_key: 'evolution_id'
+
+  belongs_to :pokemon, class_name: 'Pokemon', foreign_key: 'evolution_id', optional: true
 
   has_many :pokemon_types
   has_many :types, through: :pokemon_types
 
   # Validations
   validates :name, presence: true
-  validates :image_url, presence: true
 
+  mount_uploader :image_url, ImageUrlUploader
 end
