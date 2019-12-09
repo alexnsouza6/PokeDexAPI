63138110f5fc82533531107e231b7650014e0f27
Alex Souza <alexnsouza6@hotmail.com>
2019-12-09 18:38:14 -0300

    :art: finding user before creation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,10 +1,15 @@
 class Api::V1::UsersController < ApplicationController
   def create
-    user = User.new(user_params)
-    if user.save!
-      render json: user, status: :ok
+    user = User.find_by(account_id: user_params['account_id'])
+    if user.nil?
+      user = User.new(user_params)
+      if user.save!
+        render json: user, status: :ok
+      else
+        render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      end
     else
-      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+      render json user, status: :ok
     end
   end
 
